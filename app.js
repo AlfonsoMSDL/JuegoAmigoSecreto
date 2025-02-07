59465c3c3154beef4b13636b3fc9b26e18a8952f
Alfonso Salazar <alfonsomanuelsalazardeleon49@gmail.com>
2025-02-06 19:21:27 -0500

    Creando lista de amigos y declarando las funciones

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Creando un array para almacenar los nombres
+let listaAmigos = [];
+
+function agregarAmigo() {
+    
+}
+
+function sortearAmigo() {
+    
+}
